8658a0ac66615f9c6a3e557002f3a5e9e92a023f
Palmer Hart <palmerhart@gmail.com>
2017-02-19 15:35:41 +0000

    Bloccit-24-assignment: completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,12 @@ RSpec.describe User, type: :model do
       it "should have name and email attributes" do
           expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
       end
+      
+      it "should format users name" do
+          user.name = "bloc user"
+          user.save
+          expect(user.name).to eq "Bloc User"
+      end
   end
   
   describe "invalid user" do
@@ -36,4 +42,5 @@ RSpec.describe User, type: :model do
       end
       
   end
+  
 end
