b8a13ec6b971f0fe69c4f34d4abb1da4c81c906f
Palmer Hart <palmerhart@gmail.com>
2017-02-22 20:54:49 +0000

    Bloccit-27-checkpoint: completed

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe TopicsController, type: :controller do
     describe "GET index" do
         it "returns http success" do
             get :index
-            expect(resource).to have_http_status(:success)
+            expect(response).to have_http_status(:success)
         end
         
         it "assigns my_topic to @topic" do
@@ -28,7 +28,7 @@ RSpec.describe TopicsController, type: :controller do
         
         it "assigns my_topic to @topic" do
             get :show, {id: my_topic.id}
-            expect(assigns(:topic)).to eq([my_topic])
+            expect(assigns(:topic)).to eq(my_topic)
         end
     end
     
@@ -95,8 +95,8 @@ RSpec.describe TopicsController, type: :controller do
             
             updated_topic = assigns(:topic)
             expect(updated_topic.id).to eq my_topic.id
-            expect(updated_topic.name).to eq my_topic.name
-            expect(updated_topic.description).to eq my_topic.description
+            expect(updated_topic.name).to eq new_name
+            expect(updated_topic.description).to eq new_description
         end
         
         it "redirects to the updated topic" do
@@ -104,7 +104,7 @@ RSpec.describe TopicsController, type: :controller do
             new_description = RandomData.random_paragraph
             
             put :update, id: my_topic.id, topic: {name: new_name, description: new_description}
-            expect(response).to render_to my_topic
+            expect(response).to redirect_to(topic_path(my_topic))
         end
     end
     
